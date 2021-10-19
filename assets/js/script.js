33f6dd8b20aacad5888fb7672e96f08742919ff8
Alex Soucy <asoucy22@yahoo.com>
2021-10-19 18:30:31 -0400

    refactored code

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,15 +6,23 @@ var createTaskHandler = function(event) {
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+
+    createTaskEl(taskDataObj);
+};
+
+    var createTaskEl = function (taskDataObj) {
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
 
     var taskInfoEl = document.createElement("div");
-    taskInfoEl.className = "task-info";
-    
+    taskInfoEl.className = "task-info";    
     taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
     listItemEl.appendChild(taskInfoEl);
-
     tasksToDoEl.appendChild(listItemEl);
 };
 
